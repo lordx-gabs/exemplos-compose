01dc09adf4e2e087f6a9b140c8d3811f2a7150be
Gabriel Proença <bielsapata@gmail.com>
2026-05-13 20:31:52 -0300

    Mais alguns exemplos com image e filters, box, canvas e states do compose.

--- app/src/main/java/com/example/testecompose/MainActivity.kt
+++ app/src/main/java/com/example/testecompose/MainActivity.kt
@@ -1,31 +1,55 @@
 package com.example.testecompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.testecompose.ui.theme.TesteComposeTheme
@@ -36,7 +60,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TesteComposeTheme {
-                HomeRowColumns()
+                Login()
             }
         }
     }
@@ -47,23 +71,17 @@ fun HomeRowColumns() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.White)
+            .background(Color.White)
     ) {
         Surface(
-            modifier = Modifier
-                .size(100.dp),
+            modifier = Modifier.size(100.dp),
             color = Color.Green
-        ) {
-
-        }
+        ) {}
 
         Surface(
-            modifier = Modifier
-                .size(100.dp),
+            modifier = Modifier.size(100.dp),
             color = Color.Blue
-        ) {
-
-        }
+        ) {}
 
         Row(
             modifier = Modifier
@@ -80,6 +98,149 @@ fun HomeRowColumns() {
 
             }
         }
+    }
+}
+
+@Composable
+fun Login() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val context = LocalContext.current
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.spacedBy(
+            8.dp,
+            Alignment.CenterVertically
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Login Page",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = {
+                Text("Email")
+            },
+            maxLines = 1
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = {
+                Text("Password")
+            },
+            maxLines = 1
+        )
+
+        Button(
+            onClick = {
+                if(email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(context, "Preencha os campos!", Toast.LENGTH_SHORT).show()
+                    return@Button
+                }
+                Toast.makeText(context, "Login efetuado com sucesso.", Toast.LENGTH_SHORT).show()
+            }
+        ) {
+            Text("Login")
+        }
+    }
+}
+
+@Composable
+fun HomeCanvas() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray)
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(350.dp)
+                .background(Color.White)
+        ) {
+            drawRect(
+                color = Color.Green,
+                size = Size(
+                    width = 250f,
+                    height = 250f
+                ),
+                topLeft = Offset(
+                    x = 50f,
+                    y = 50f
+                )
+            )
+
+            drawOval(
+                color = Color.Green,
+                size = Size(
+                    width = 150f,
+                    height = 150f
+                ),
+                topLeft = Offset(
+                    x = 50f,
+                    y = 500f
+                )
+            )
+
+            drawLine(
+                color = Color.Green,
+                start = Offset(
+                    x = 500f,
+                    y = 500f
+                ),
+                end = Offset(
+                    x = 700f,
+                    y = 700f
+                ),
+                strokeWidth = 8f
+            )
+        }
+    }
+}
+
+@Composable
+fun HomeImage() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            modifier = Modifier
+                .width(250.dp)
+                .clip(
+                    shape = RoundedCornerShape(16.dp)
+                ),
+            painter = painterResource(R.drawable.img),
+            contentDescription = "Imagem da Pomni",
+            contentScale = ContentScale.None,
+            colorFilter = ColorFilter.colorMatrix(
+                colorMatrix = ColorMatrix().apply {
+                    setToSaturation(1.7f)
+                    setToRotateRed(1f)
+                    setToRotateGreen(2f)
+                }
+            )
+        )
     }
 }
 
@@ -132,6 +293,48 @@ fun HomeScaffold() {
             )
         }
     }
+}
+
+@Composable
+fun HomeBox() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .size(200.dp)
+                .background(Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(Color.Black)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .background(Color.Green)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(10.dp)
+                            .background(Color.Red)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeBoxPreview() {
+    HomeImage()
 }
 
 
