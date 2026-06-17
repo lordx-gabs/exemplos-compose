0945e386512c370fca9e789e43dbada96407d5d4
Gabriel Proença <bielsapata@gmail.com>
2026-06-17 19:43:51 -0300

    Colocado algums tipos de lazy layout, column e grid, e a exibição de um dialog simples.

--- app/src/main/java/com/example/testecompose/MainActivity.kt
+++ app/src/main/java/com/example/testecompose/MainActivity.kt
@@ -5,22 +5,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -30,6 +41,9 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -38,29 +52,35 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import com.example.testecompose.ui.ThemeViewModel
 import com.example.testecompose.ui.navigation.AppNavHost
 import com.example.testecompose.ui.theme.TesteComposeTheme
 import org.koin.androidx.compose.koinViewModel
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             val themeViewModel = koinViewModel<ThemeViewModel>()
-            val darkTheme by themeViewModel.darkTheme.collectAsStateWithLifecycle(isSystemInDarkTheme())
+            val darkTheme by themeViewModel.darkTheme.collectAsStateWithLifecycle(
+                isSystemInDarkTheme()
+            )
             TesteComposeTheme(
                 darkTheme = darkTheme
             ) {
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    AppNavHost(
-                        rememberNavController()
-                    )
-                }
+//                Surface(
+//                    modifier = Modifier.fillMaxSize()
+//                ) {
+//                    AppNavHost(
+//                        rememberNavController()
+//                    )
+//                }
+                HomeScaffold()
             }
         }
     }
@@ -174,6 +194,9 @@ fun PagerExample(modifier: Modifier = Modifier) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScaffold() {
+    var showDialog by remember {
+        mutableStateOf(false)
+    }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -181,10 +204,14 @@ fun HomeScaffold() {
                     Text(text = "Teste")
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.Blue
+                    containerColor = Color.Cyan
                 ),
                 actions = {
                     Icon(
+                        modifier = Modifier
+                            .clickable {
+                                showDialog = true
+                            },
                         imageVector = Icons.Rounded.Home,
                         contentDescription = null
                     )
@@ -193,32 +220,78 @@ fun HomeScaffold() {
         },
         bottomBar = {
             BottomAppBar(
-                containerColor = Color.Blue
+                containerColor = Color.Cyan
             ) {
 
             }
         }
     ) { innerPadding ->
-        Column(
+        val scrollState = rememberScrollState()
+        val count = 15
+        LazyColumn(
             modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(innerPadding),
         ) {
-            Text(
-                text = "Teste1",
-                fontSize = 20.sp
-            )
-            Text(
-                text = "Teste2",
-                fontSize = 20.sp
-            )
-            Text(
-                text = "Teste3",
-                fontSize = 20.sp
-            )
+            items(count) { position ->
+                GetColumnOfTexts()
+            }
         }
+//        LazyVerticalGrid(
+//            modifier = Modifier
+//                .padding(innerPadding),
+//            columns = GridCells.Fixed(2)
+//        ) {
+//            items(count) { position ->
+//                GetColumnOfTexts()
+//            }
+//        }
+
+//        LazyVerticalStaggeredGrid(
+//            modifier = Modifier
+//                .padding(innerPadding),
+//            columns = StaggeredGridCells.Fixed(2)
+//        ) {
+//            items(count) { position ->
+//                GetColumnOfTexts()
+//            }
+//        }
+
+        if(showDialog) {
+            Dialog(
+                onDismissRequest = {
+                    showDialog = false
+                }
+            ) {
+                Card(
+                    modifier = Modifier.size(200.dp)
+                ) {
+                    Text("TesteDialog")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun GetColumnOfTexts() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Teste1",
+            fontSize = 20.sp
+        )
+        Text(
+            text = "Teste2",
+            fontSize = 20.sp
+        )
+        Text(
+            text = "Teste3",
+            fontSize = 20.sp
+        )
     }
 }
 
@@ -261,7 +334,7 @@ fun HomeBox() {
 @Preview
 @Composable
 fun HomeBoxPreview() {
-    HomeBox()
+    HomeScaffold()
 }
 
 
